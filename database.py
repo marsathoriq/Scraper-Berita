c15182d737f0c93fb74e76ea628bf45bff58837a
imdar1 <isamujahid.im@gmail.com>
2021-06-03 22:51:19 +0700

    Update predict function

---
+++ database.py
@@ -0,0 +1,71 @@
+import sqlalchemy
+import os
+
+def init_connection_engine():
+    db_config = {
+        # [START cloud_sql_mysql_sqlalchemy_limit]
+        # Pool size is the maximum number of permanent connections to keep.
+        "pool_size": 5,
+        # Temporarily exceeds the set pool_size if no connections are available.
+        "max_overflow": 2,
+        # The total number of concurrent connections for your application will be
+        # a total of pool_size and max_overflow.
+        # [END cloud_sql_mysql_sqlalchemy_limit]
+
+        # [START cloud_sql_mysql_sqlalchemy_backoff]
+        # SQLAlchemy automatically uses delays between failed connection attempts,
+        # but provides no arguments for configuration.
+        # [END cloud_sql_mysql_sqlalchemy_backoff]
+
+        # [START cloud_sql_mysql_sqlalchemy_timeout]
+        # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
+        # new connection from the pool. After the specified amount of time, an
+        # exception will be thrown.
+        "pool_timeout": 30,  # 30 seconds
+        # [END cloud_sql_mysql_sqlalchemy_timeout]
+
+        # [START cloud_sql_mysql_sqlalchemy_lifetime]
+        # 'pool_recycle' is the maximum number of seconds a connection can persist.
+        # Connections that live longer than the specified amount of time will be
+        # reestablished
+        "pool_recycle": 1800,  # 30 minutes
+        # [END cloud_sql_mysql_sqlalchemy_lifetime]
+
+    }
+
+    if os.environ.get("DB_HOST"):
+        return init_tcp_connection_engine(db_config)
+    else:
+        return init_unix_connection_engine(db_config)
+
+def init_unix_connection_engine(db_config):
+    # [START cloud_sql_mysql_sqlalchemy_create_socket]
+    # Remember - storing secrets in plaintext is potentially unsafe. Consider using
+    # something like https://cloud.google.com/secret-manager/docs/overview to help keep
+    # secrets secret.
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+    cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
+
+    pool = sqlalchemy.create_engine(
+        # Equivalent URL:
+        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+        sqlalchemy.engine.url.URL.create(
+            drivername="mysql+pymysql",
+            username=db_user,  # e.g. "my-database-user"
+            password=db_pass,  # e.g. "my-database-password"
+            database=db_name,  # e.g. "my-database-name"
+            query={
+                "unix_socket": "{}/{}".format(
+                    db_socket_dir,  # e.g. "/cloudsql"
+                    cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+            }
+        ),
+        **db_config
+    )
+    # [END cloud_sql_mysql_sqlalchemy_create_socket]
+
+    return pool
+
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import os
+import re
+import string
+import joblib
+import operator
+import numpy as np
 import tensorflow as tf
 import pandas as pd
+
 from google.cloud import storage
 from datetime import datetime
 from detik import get_detik_dataframe_from_date
 from kompas import get_kompas_dataframe_from_date
+
 
 ## Download model
 def download_model(bucket_name, source_dir, destination_folder, model_version):
@@ -50,7 +57,7 @@ def check_len(data):
   return len(data.split())
 
 # bersihkan teks
-def clean_text(text):
+def clean_text(text, stop_words):
     '''Make text lowercase, remove text in square brackets, remove punctuation and remove words containing numbers.'''
     text = str(text)
     text = text.lower()
@@ -63,15 +70,23 @@ def clean_text(text):
     text = ' '.join([word for word in text.split() if word.lower() not in stop_words])
     return text
 
-def vectorize_verif(data):
+def vectorize_verif(data, CountVectorizer):
     bow_data = CountVectorizer.transform(data['content'])
     bow_data = pd.DataFrame.sparse.from_spmatrix(bow_data,columns=CountVectorizer.get_feature_names())
     return bow_data
 
-def predict_data(data):
-    data['content'] = data['content'].apply(clean_text)
-    bow_data = vectorize_verif(data)
-    return model.predict(bow_data).round()
+def predict_data(data, CountVectorizer, stop_words, model):
+    data['content'] = data['content'].apply(clean_text, stop_words=stop_words)
+    bow_data = vectorize_verif(data, CountVectorizer)
+    return model.predict(bow_data)
+
+def change_label(value):
+    if value > 0.6:
+        return 1
+    elif value < 0.4:
+        return -1
+    else:
+        return 0
 
 def full_predict(event, context):
     ## Get news
@@ -82,28 +97,98 @@ def full_predict(event, context):
 
     ## Preprocess
     news_data['content'] = detik_news['content'].apply(get_content)
-    news_data['Len'] = news_data['content'].apply(check_len)
-    news_data = news_data[news_data['Len'] > 10].reset_index(drop=True)
+    # news_data['Len'] = news_data['content'].apply(check_len)
+    # news_data = news_data[news_data['Len'] > 10].reset_index(drop=True)
 
-    ## Load tensorflow model
+    ## Load tensorflow model for sentiment analysis
     dest_folder = '/tmp/'
-    download_dir = download_model(
+    download_dir_sentiment = download_model(
         bucket_name = os.environ['BUCKET_NAME'],
-        source_dir = 'model',
+        source_dir = 'Sentiment/model',
         destination_folder = dest_folder,
         model_version = '1'
     )
 
-    dest_vectorizer = os.path.join(dest_folder, 'CountVectorizer.pkl')
-    download_vectorizer = download_blob(
+    dest_vectorizer_sentiment = os.path.join(dest_folder, 'CountVectorizerSentiment.pkl')
+    download_blob(
         bucket_name = os.environ['BUCKET_NAME'],
-        source_blob_name = 'CountVectorizer.pkl',
-        destination_file_name = dest_folder
+        source_blob_name = 'Sentiment/CountVectorizer.pkl',
+        destination_file_name = dest_vectorizer_sentiment
     )
 
-    CountVectorizer = joblib.load(dest_vectorizer)  
-    stop_words = open("stopword.txt", "r").read().split()
-    model = tf.keras.models.load_model('model')
+    ## Load tf model for topic classification
+    download_dir_jakarta = download_model(
+        bucket_name = os.environ['BUCKET_NAME'],
+        source_dir = 'Jakarta/model',
+        destination_folder = dest_folder,
+        model_version = '2'
+    )
 
-    pred = predict_data(news_data)
-    print(pred)
+    dest_vectorizer_jakarta = os.path.join(dest_folder, 'CountVectorizerJakarta.pkl')
+    download_blob(
+        bucket_name = os.environ['BUCKET_NAME'],
+        source_blob_name = 'Jakarta/CountVectorizer.pkl',
+        destination_file_name = dest_vectorizer_jakarta
+    )
+
+    ## Load tf model for text classification
+    download_dir_topic = download_model(
+        bucket_name = os.environ['BUCKET_NAME'],
+        source_dir = 'Topic/model',
+        destination_folder = dest_folder,
+        model_version = '3'
+    )
+
+    dest_vectorizer_topic = os.path.join(dest_folder, 'CountVectorizerTopic.pkl')
+    download_blob(
+        bucket_name = os.environ['BUCKET_NAME'],
+        source_blob_name = 'Topic/CountVectorizer.pkl',
+        destination_file_name = dest_vectorizer_topic
+    )
+
+    ## Predict jakarta
+    CountVectorizer = joblib.load(dest_vectorizer_jakarta)
+    stop_words = open("stopword.txt", "r").read().split()
+    model = tf.keras.models.load_model(download_dir_jakarta)
+
+    pred = predict_data(news_data, CountVectorizer, stop_words, model).round()
+    news_data['jakarta'] = pred
+
+    ## Predict topic
+    CountVectorizer = joblib.load(dest_vectorizer_topic)
+    stop_words = open("stopword.txt", "r").read().split()
+    model = tf.keras.models.load_model(download_dir_topic)
+
+    pred = predict_data(news_data, CountVectorizer, stop_words, model)
+    news_data['topic'] = np.argmax(pred, axis=1)
+
+    ## Predict sentiment
+    CountVectorizer = joblib.load(dest_vectorizer_sentiment)  
+    model = tf.keras.models.load_model(download_dir_sentiment)
+
+    pred = predict_data(news_data, CountVectorizer, stop_words, model)
+
+    vchangeLabel = np.vectorize(change_label)
+    news_data['sentiment'] = vchangeLabel(pred)
+    
+    print(news_data)
+
+    data_positif = news_data[news_data['sentiment'] == 1]
+    data_negatif = news_data[news_data['sentiment'] == -1]
+
+    cnt_tags_positive = {}
+    for row in data_positif.itertuples():
+        for tag in row.tags:
+            cnt_tags_positive[tag] = cnt_tags_positive.get(tag, 0) + 1
+
+    cnt_tags_negative = {}
+    for row in data_negatif.itertuples():
+        for tag in row.tags:
+            cnt_tags_negative[tag] = cnt_tags_negative.get(tag, 0) + 1
+    
+    sorted_cnt_positive = sorted(cnt_tags_positive.items(), key=operator.itemgetter(1),reverse=True)
+    sorted_cnt_negative = sorted(cnt_tags_negative.items(), key=operator.itemgetter(1),reverse=True)
+    print(sorted_cnt_positive)
+    print(sorted_cnt_negative)
+
+    ## TODO:Insert to DB
